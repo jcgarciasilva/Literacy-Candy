1f51e7eca938dffd7410358039e3409d8ef551c0
Joao GARCIA <jgarcia.ext@datasolution.fr>
2023-05-16 14:35:06 +0000

    add new controls

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { I18nModule } from 'nestjs-i18n'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import * as path from 'path'
 
 @Module({
-  imports: [],
+  imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: 'pt',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      viewEngine: 'hbs',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
