e3a54dd8a4d92311de44bd3c77af50de3534155a
Joao GARCIA <jgarcia.ext@datasolution.fr>
2023-05-19 12:27:36 +0000

    add new funcionality to i18n

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,51 +1,68 @@
-import { Controller, Get, Req, Res } from '@nestjs/common'
+import { Controller, Get, Param, Req, Res } from '@nestjs/common'
+import { throws } from 'assert'
 import { Request, Response } from 'express'
+import { I18n, I18nContext, I18nLang, I18nService } from 'nestjs-i18n'
 import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get(['index', '/'])
-  index(@Req() req: Request, @Res() res: Response) {
+  @Get(['/', '/:lang'])
+  index(
+    @Req() req: Request,
+    @Res() res: Response,
+    @I18n() i18n: I18nContext,
+    @I18nLang() lang: string
+  ) {
+    console.log(lang)
     return res.status(200).render('index', {
       layout: 'main',
       url: req.url,
       gradient: true,
-      title: 'Literacy Candy',
+      lang: lang ? 'pt' : i18n.lang,
+      title: i18n.t('site.home'),
     })
   }
 
-  @Get(['team'])
-  team(@Req() req: Request, @Res() res: Response) {
+  @Get(['team', ':lang/team'])
+  team(
+    @Req() req: Request,
+    @Res() res: Response,
+    @I18n() i18n: I18nContext,
+    @I18nLang() lang: string
+  ) {
     return res.status(200).render('team', {
       layout: 'main',
       url: req.url,
       gradient: true,
+      lang: lang ? 'pt' : i18n.lang,
       title: 'Literacy Candy',
     })
   }
 
-  @Get('novelas')
-  novelas(@Res() res: Response) {
+  @Get(['novelas', ':lang/novelas'])
+  novelas(@Res() res: Response, @I18n() i18n: I18nContext, @I18nLang() lang: string) {
     return res.render('novelas', {
       layout: 'main',
       title: 'Literacy Candy',
+      lang: lang ? 'pt' : i18n.lang,
       books: this.appService.getNovels(),
     })
   }
 
-  @Get('receitas')
-  receitas(@Res() res: Response) {
+  @Get(['receitas', ':lang/receitas'])
+  receitas(@Res() res: Response, @I18n() i18n: I18nContext, @I18nLang() lang: string) {
     return res.render('receitas', {
       layout: 'main',
       title: 'Literacy Candy',
+      lang: lang ? 'pt' : i18n.lang,
       books: this.appService.getRecepies(),
     })
   }
 
-  @Get('poesias')
-  poesias(@Res() res: Response) {
+  @Get(['poesias', ':lang/poesias'])
+  poesias(@Res() res: Response, @I18n() i18n: I18nContext, @I18nLang() lang: string) {
     return res.render('poesias', {
       layout: 'main',
       title: 'Literacy Candy',
@@ -53,8 +70,8 @@ export class AppController {
     })
   }
 
-  @Get('contos')
-  contos(@Res() res: Response) {
+  @Get(['contos', ':lang/contos'])
+  contos(@Res() res: Response, @I18n() i18n: I18nContext, @I18nLang() lang: string) {
     return res.render('contos', {
       layout: 'main',
       title: 'Literacy Candy',
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common'
-import { I18nModule } from 'nestjs-i18n'
+import { I18nModule, QueryResolver, I18nJsonLoader } from 'nestjs-i18n'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import * as path from 'path'
@@ -10,9 +10,12 @@ import * as path from 'path'
       fallbackLanguage: 'pt',
       loaderOptions: {
         path: path.join(__dirname, '/i18n/'),
+        includeSubfolders: true,
         watch: true,
       },
+      loader: I18nJsonLoader,
       viewEngine: 'hbs',
+      resolvers: [{ use: QueryResolver, options: ['lang'] }],
     }),
   ],
   controllers: [AppController],
