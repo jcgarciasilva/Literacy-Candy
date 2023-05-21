a670b6507c80124604bda613cb83b5c47f6d38f8
Joao GARCIA <jgarcia.ext@datasolution.fr>
2023-05-21 16:35:36 +0000

    new keys

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,7 +8,6 @@ export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly i18nService: I18nService
-    
   ) {}
 
   @Get(['/', '/:lang'])
@@ -25,10 +24,16 @@ export class AppController {
       gradient: true,
       lang: lang ? 'pt' : i18n.lang,
       title: i18n.t('site.title'),
+      contact: i18n.t('site.contact'),
+      main_title: i18n.t('site.main.title', { lang }),
+      main_text: i18n.t('site.main.main_text',{ lang }),
+      main_sub_text: i18n.t('site.main.sub_text', { lang }),
+      main_button: i18n.t('site.main.button', { lang }),
       whoweare: i18n.t('site.whoweare.title', { lang }),
       whoweare_title1: i18n.t('site.whoweare.title1', { lang }),
       whoweare_description1: i18n.t('site.whoweare.description1', { lang }),
       whoweare_description2: i18n.t('site.whoweare.description2', { lang }),
+      mission_header: i18n.t('site.mission.header', { lang }),
       mission_title: i18n.t('site.mission.title', { lang }),
       mission_description: i18n.t('site.mission.description', { lang }),
     })
